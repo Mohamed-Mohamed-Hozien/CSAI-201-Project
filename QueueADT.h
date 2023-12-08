6575583a282ca349a9c571b7f108ecad86604359
Mohamed Tag <mohamedtag264@gmail.com>
2023-12-08 17:49:40 +0200

    Queue Abstract

---
+++ QueueADT.h
@@ -0,0 +1,28 @@
+
+#ifndef QUEUEADT_H
+#define QUEUEADT_H
+
+#include "Node.h"
+
+template <typename T>
+class QueueADT {
+private:
+    Node* front; 
+    Node* rear;  
+    int size; 
+
+public:
+    QueueADT() : front(nullptr), rear(nullptr), size(0) {}
+
+    virtual void enqueue(const T& data);
+
+    virtual bool dequeue(T& data);
+
+    virtual bool isEmpty() const;
+
+    virtual int getSize() const;
+
+    virtual bool getFront(T& data) const;
+};
+
+#endif
