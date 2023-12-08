b0ea03449aef6cb8d37e3e020e81614a2dfffa11
mohamedtag04 <mohamedtag264@gmail.com>
2023-12-08 17:53:31 +0200

    template fix

--- QueueADT.h
+++ QueueADT.h
@@ -4,15 +4,15 @@
 #include "Node.h"
 
 template <typename T>
-class QueueADT {
+class QueueADT
+{
 public:
     // virtual void enqueue(const T& data, int pri);
     // virtual void enqueue(const T& data);
-    virtual bool dequeue(T& data) = 0;
+    virtual bool dequeue(T &data) = 0;
     virtual bool isEmpty() const = 0;
     virtual int getSize() const = 0;
-    virtual bool getFront(T& data) const = 0;
-
+    virtual bool getFront(T &data) const = 0;
 };
 
 #endif
